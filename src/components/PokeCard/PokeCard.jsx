ef035cb5af03dcf64f873d64a91e79b9fcd738af
Mariia Kuzhydlo <kushnirmariia.m@gmail.com>
2022-09-27 16:25:34 +0300

    task added

--- src/components/PokeCard/PokeCard.jsx
+++ src/components/PokeCard/PokeCard.jsx
@@ -2,6 +2,67 @@ import React from "react";
 import s from "./PokeCard.module.css"
 
 const PokeCard = ({ pokemon, loading, infoPokemon }) => {
+
+	const getTypeStyle = (type) => {
+		let backgroundColor = "";
+		switch (type) {
+			case "grass":
+				backgroundColor = "#9bcc50";
+				break;
+			case "poison":
+				backgroundColor = "#b97fc9";
+				break;
+			case "fire":
+				backgroundColor = "#fd7d24";
+				break;
+			case "flying":
+				backgroundColor = "#3dc7ef";
+				break;
+			case "water":
+				backgroundColor = "#4592c4";
+				break;
+			case "bug":
+				backgroundColor = "#729f3f";
+				break;
+			case "normal":
+				backgroundColor = "#a4acaf";
+				break;
+			case "electric":
+				backgroundColor = "#eed535";
+				break;
+			case "ground":
+				backgroundColor = "#ab9842";
+				break;
+			case "fairy":
+				backgroundColor = "#fdb9e9";
+				break;
+			case "fighting":
+				backgroundColor = "#d56723";
+				break;
+			case "psychic":
+				backgroundColor = "#f366b9";
+				break;
+			case "rock":
+				backgroundColor = "#a38c21";
+				break;
+			case "steel":
+				backgroundColor = "#9eb7b8";
+				break;
+			case "ghost":
+				backgroundColor = "#7b62a3";
+				break;
+			case "ice":
+				backgroundColor = "#51c4e7";
+			case "dragon":
+				backgroundColor = "#f16e57";
+
+			default:
+				backgroundColor = "#000";
+				break;
+		}
+		return { backgroundColor, color: "#FFF" };
+	};
+
 	return (
 		<>
 			{
@@ -17,7 +78,8 @@ const PokeCard = ({ pokemon, loading, infoPokemon }) => {
 									<div className={s.types}>
 										{item.types.map((type, index) => {
 											return (
-												<span className={s.type} key={index}>
+												<span className={s.type} key={index}
+													style={getTypeStyle(type.type.name)}>
 													{type.type.name}
 												</span>
 											)
